31ee98efbdbf8512af75e1bd74c2e5105c43e769
Matheus Duarte Albuquerque <90364847+Mathdal002@users.noreply.github.com>
2023-02-16 17:56:56 -0300

    App de Eventos
    Um sistema web de eventos, onde eu consigo cadastrar eventos e os convidados que participarem de cada evento.

---
+++ EventoApp/src/test/java/br/gov/go/sefaz/EventoApp/EventoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.go.sefaz.EventoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
