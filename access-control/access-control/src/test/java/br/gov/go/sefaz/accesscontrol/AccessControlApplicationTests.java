855c2b451404231eb928ced07dc7726a93ab93fa
Matheus Duarte Albuquerque <90364847+Mathdal002@users.noreply.github.com>
2023-03-06 15:29:57 -0300

    Projeto de cadastro de Usuários
    Criei um projeto simples de cadastro de Usuários com Spring Boot e Maven.

---
+++ access-control/access-control/src/test/java/br/gov/go/sefaz/accesscontrol/AccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.go.sefaz.accesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ access-control/src/main/java/br/gov/go/sefaz/accesscontrol/AccessControlApplication.java
@@ -0,0 +1,14 @@
+package br.gov.go.sefaz.accesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class AccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlApplication.class, args);
+	}
+
+}
