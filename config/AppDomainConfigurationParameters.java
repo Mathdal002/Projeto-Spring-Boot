7a8be0f4a8eb7b1280617e3e2ad49b2baa0cd6ed
Matheus Duarte Albuquerque <90364847+Mathdal002@users.noreply.github.com>
2023-03-06 15:18:32 -0300

    Delete config directory

--- config/AppDomainConfigurationParameters.java
+++
@@ -1,9 +0,0 @@
-package br.gov.go.sefaz.hrs.config;
-
-public class AppDomainConfigurationParameters {
-
-	public static final String PACKAGE_REPOSITORY_JPA			=	"br.gov.go.sefaz.hrs.repository.jpa";
-	public static final String PACKAGE_REPOSITORY_JDBC			=	"br.gov.go.sefaz.hrs.repository.jdbc.impl";
-	public static final String PACKAGE_SERVICE					= 	"br.gov.go.sefaz.hrs.service";
-	public static final String PACKAGE_VALIDATION				= 	"br.gov.go.sefaz.hrs.validation";
-}
